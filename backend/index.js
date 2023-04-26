07b5a7ac762c52af81671244039bfbd2415114ce
DaniloNogueira-Silva <dansilva730x@gmail.com>
2023-04-26 12:22:23 -0300

    fix: Ajustando ortografia

--- backend/index.js
+++ backend/index.js
@@ -15,11 +15,12 @@ connection
 
 // models
 const User = require('./models/User');
-const Category = require('./models/Category')
-const Product = require('./models/Product')
-const Order = require('./models/Order')
-const Budget = require('./models/Budget')
-const BuyList = require('./models/BuyList')
+const Category = require('./models/Category');
+const Product = require('./models/Product');
+const Order = require('./models/Order');
+const Budget = require('./models/Budget');
+const BuyList = require('./models/BuyList');
+
 //middlewares
 const app = express();
 app.use(express.json());
