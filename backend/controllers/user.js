8250e81f18f41f707e05fb8523423dd8fb192fd2
DaniloNogueira-Silva <dansilva730x@gmail.com>
2023-04-27 11:14:12 -0300

    feat: Criar controller para usuários e login

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,28 +1,105 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
 
 class UserController {
+
+  async get(req, res){
+    const users = await User.findAll()
+    res.status(200).send(users);
+  }
+
+  async findById(req, res) {
+    const id = req.params.id
+    const user = await User.findOne({where:{id:id}})
+    if(user == undefined){
+      res.status(404).send({mesg:'Usuário não encontrado'})
+    }else{
+      res.status(200).send(user);
+    }
+  }
+
   async create(req, res) {
     try {
-      let name = req.body.name;
-      let email = req.body.email;
-      let password = req.body.password;
-      let isAdmin = req.body.isAdmin;
+      const name = req.body.name;
+      const email = req.body.email;
+      const password = req.body.password;
+      const isAdmin = req.body.isAdmin;
 
-      var salt = bcrypt.genSaltSync(10);
-      var hash = bcrypt.hashSync(password, salt);
+      await User.findOne({ where: { email: email } }).then((user) => {
+        if (user == undefined) {
+          const salt = bcrypt.genSaltSync(10);
+          const hash = bcrypt.hashSync(password, salt);
 
-      await User.create({
-        name,
-        email,
-        password: hash,
-        isAdmin,
+          User.create({
+            name,
+            email,
+            password: hash,
+            isAdmin,
+          });
+          res.status(200).send("Usuário cadastrado");
+        }else {
+          res.status(406).send({err: "Email já cadastrado"});
+        }
       });
-      res.status(200);
-      res.send("Usuário cadastrado");
     } catch (error) {
       res.status(401).send(error);
     }
+  };
+
+  async remove(req,res){
+    const id = req.params.id
+    const user = await User.findOne({where:{id:id}})
+    if(user == undefined){
+      res.status(404).send({msg:'Usuário não encontrado'})
+      return
+    }else{
+      res.status(200).send(user);
+      const result = await User.destroy({where: {id:id}})
+    }
+  };
+
+  async update(req, res){
+    try {
+      const {name, email, password, isAdmin} = req.body
+      const id = req.params.id
+
+      const salt = bcrypt.genSaltSync(10);
+      const hash = bcrypt.hashSync(password, salt);
+
+      const result = await User.update({name: name, email: email, password: hash, isAdmin: isAdmin}, {
+        where: {
+          id: id
+        }
+      });
+      res.status(200).send()
+  
+    } catch(error) {
+      res.status(401).send(error)
+    }
+      
+  }
+
+  async login(req,res){
+      const {email, password} = req.body
+
+      const user = await User.findOne({where: { email: email
+      }});
+
+      if(user!= undefined){
+
+        const result = await bcrypt.compare(password, user.password);
+
+        if(result){
+          const token = jwt.sign({email: user.email, isAdmin: user.isAdmin}, process.env.TOKEN_SECRET);
+
+          res.status(200).send({token: token});
+        }else {
+          res.status(406).send("Credenciais incorretas");
+        }
+      }else{
+        res.send("Erro.")
+      }
   }
 }
 
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -4,13 +4,24 @@ const UserController = require('../controllers/user');
 const CategoryController = require('../controllers/category');
 const ProductController = require("../controllers/product")
 
+//category
 router.post('/admin/user', UserController.create);
 router.get('/admin/category', CategoryController.showAll)
 router.post('/admin/category', CategoryController.create);
 router.delete('/admin/deleteCategory/:id', CategoryController.remove)
+
+//product
 router.get('/admin/product', ProductController.showAll)
 router.post('/admin/product', ProductController.create)
 router.delete('/admin/deleteProduct/:id', ProductController.remove)
 router.post('/admin/editProduct/:id', ProductController.edit)
+
+//user
+router.post('/admin/user', UserController.create);
+router.get('/admin/user', UserController.get);
+router.get('/admin/user/:id', UserController.findById);
+router.delete('/admin/user/:id', UserController.remove);
+router.post('/admin/user/:id', UserController.update);
+router.post('/auth', UserController.login);
 
 module.exports = router;
