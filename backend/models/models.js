aa634c850023df055ef2774daa22a96d2b2f0b56
DaniloNogueira-Silva <dansilva730x@gmail.com>
2023-05-15 19:56:41 -0300

    att models

--- backend/models/models.js
+++ backend/models/models.js
@@ -1,21 +1,14 @@
-const { Budget, BudgetCompany, BudgetPrice, BudgetItems } = require("./Budget");
-const BuyList = require("./BuyList");
-const Category = require("./Category");
-const Measure = require("./Measure");
-const { Order, OrderItem } = require("./Order");
-const Product = require("./Product");
-const { PasswordToken, User } = require("./User");
-
-export default {
-  Budget,
-  BudgetCompany,
-  BudgetItems,
-  BudgetPrice,
-  User,
-  PasswordToken,
-  Order,
-  OrderItem,
-  Product,
-  Category,
-  Measure,
-} = models;
+module.exports = models = () => {
+  const {
+    Budget,
+    BudgetCompany,
+    BudgetPrice,
+    BudgetItems,
+  } = require("./Budget");
+  const BuyList = require("./BuyList");
+  const Category = require("./Category");
+  const Measure = require("./Measure");
+  const { Order, OrderItem } = require("./Order");
+  const Product = require("./Product");
+  const { PasswordToken, User } = require("./User");
+};
